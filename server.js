bcb4f918d560989d57da22f7c3b3a574173b75d8
Ons Hadriche <ounshadriche@gmail.com>
2022-09-18 22:06:03 +0200

    Set up folders

--- server.js
+++ server.js
@@ -10,20 +10,20 @@ app.use(cors({ credentials: true, origin: [process.env.WEB_APP_URL] }));
 
 const PORT = process.env.PORT || 5000;
 
-// mongoose
-//   .connect(process.env.Data_Base_URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log("Successfully connected to MongoDB");
-//     app.listen(PORT, () => {
-//       console.log(`Server is listening on port ${PORT}`);
-//     });
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
-app.listen(PORT, () => {
-  console.log(`Server is listening ${PORT}`);
-});
+mongoose
+  .connect(process.env.Data_Base_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Successfully connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+// app.listen(PORT, () => {
+//   console.log(`Server is listening ${PORT}`);
+// });
